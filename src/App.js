9271fc52e469005e9f549257f875fbc5eaf3ae15
komanseok <msggun65@gmail.com>
2020-12-12 16:17:33 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gray-background">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <h2>Hi</h2>
+    </div>
+  );
+}
+
+export default App;
